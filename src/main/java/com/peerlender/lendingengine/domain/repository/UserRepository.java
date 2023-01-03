27b390b17f49f737402f673a0e1643ebd74ab5ec
nanna <navkishoredas@gmail.com>
2023-01-03 17:58:01 +0530

    not working fix the problem h2 grammar

--- src/main/java/com/peerlender/lendingengine/domain/repository/UserRepository.java
+++ src/main/java/com/peerlender/lendingengine/domain/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.peerlender.lendingengine.domain.repository;
 
 import com.peerlender.lendingengine.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//implementing from crud to get the methods
 public interface UserRepository extends JpaRepository< User, Long > {
 
 
