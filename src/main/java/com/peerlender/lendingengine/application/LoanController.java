5116049f536bb33f8095ce36237ee4da002d5d06
nanna <navkishoredas@gmail.com>
2023-01-04 23:09:07 +0530

    not working fix the problem h2 grammar

--- src/main/java/com/peerlender/lendingengine/application/LoanController.java
+++ src/main/java/com/peerlender/lendingengine/application/LoanController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//controller class !
 @RestController
 public class LoanController {
     
