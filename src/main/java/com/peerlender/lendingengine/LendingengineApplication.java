99955f718955c496724fd7c7148a63c856056445
nanna <navkishoredas@gmail.com>
2023-01-06 23:22:26 +0530

    not working fix the problem h2 grammar

--- src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
+++ src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
@@ -20,7 +20,7 @@ public class LendingengineApplication implements CommandLineRunner {
 	@Override
 	public void run ( String... args ) throws Exception {
 //		creatting bootstrap data
-		
+//		repository saving data
 		userRepository.save ( new User ( 1 , "Nanna" , "Dash" , 30 , "Software developer" ) );
 		userRepository.save ( new User ( 2 , "bapu" , "premanand" , 32 , "social worker" ) );
 		userRepository.save ( new User ( 2 , "sainrupendra " , "dash" , 35 , "Manager" ) );
