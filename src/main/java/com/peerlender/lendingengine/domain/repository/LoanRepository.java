646f367a17fb15a9db2da4cdc5eb51fc0e2e2af6
nanna <navkishoredas@gmail.com>
2023-01-06 23:24:34 +0530

    not working fix the problem h2 grammar fix it

--- src/main/java/com/peerlender/lendingengine/domain/repository/LoanRepository.java
+++ src/main/java/com/peerlender/lendingengine/domain/repository/LoanRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface LoanRepository extends JpaRepository< LoanApplication, Long > {
 
 
-
+//jPA repository implementing saving update delete find method
 
 }
