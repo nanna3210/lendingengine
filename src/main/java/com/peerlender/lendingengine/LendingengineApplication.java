f813ee551fa6af46162a932ada7653d35cfb473a
nanna <navkishoredas@gmail.com>
2023-01-08 22:35:57 +0530

    //main Application class

--- src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
+++ src/main/java/com/peerlender/lendingengine/LendingengineApplication.java
@@ -19,7 +19,7 @@ public class LendingengineApplication implements CommandLineRunner {
 	
 	@Override
 	public void run ( String... args ) throws Exception {
-//		creatting bootstrap data
+//		creating bootstrap data
 //		repository saving data
 		userRepository.save ( new User ( 1 , "Nanna" , "Dash" , 30 , "Software developer" ) );
 		userRepository.save ( new User ( 2 , "bapu" , "premanand" , 32 , "social worker" ) );
